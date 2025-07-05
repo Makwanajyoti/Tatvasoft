413b2c6475315b72f7c2ab193d827efced89a19e
Makwanajyoti <jyotimakavana221@gmail.com>
2025-07-05 16:18:06 +0530

    Add files via upload

---
+++ DAY 3/RegistrationApp.Api/Program.cs
@@ -0,0 +1,24 @@
+using RegistrationApp.Repositories;
+using RegistrationApp.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services
+builder.Services.AddSingleton<IStudentRepository, StudentRepository>();
+builder.Services.AddScoped<StudentService>();
+builder.Services.AddSingleton<StudentService>();
+
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();

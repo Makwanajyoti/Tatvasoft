ecd3afdf9dae0cc61ba811ac6312f2301f812047
Makwanajyoti <jyotimakavana221@gmail.com>
2025-07-05 16:26:42 +0530

    Add files via upload

---
+++ DAY 3/RegistrationApp.Services/StudentService.cs
@@ -0,0 +1,27 @@
+﻿
+using RegistrationApp.Entities;
+using RegistrationApp.Repositories;
+using System.Collections.Generic;
+
+namespace RegistrationApp.Services
+{
+    public class StudentService
+    {
+        private readonly IStudentRepository _repository;
+
+        public StudentService(IStudentRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public List<Student> GetAll()
+        {
+            return _repository.GetAll();
+        }
+
+        public void Add(Student student)
+        {
+            _repository.Add(student);
+        }
+    }
+}

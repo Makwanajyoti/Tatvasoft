47137768d837faf7926df0e36984f7578fda4215
Makwanajyoti <jyotimakavana221@gmail.com>
2025-07-05 16:26:15 +0530

    Add files via upload

---
+++ DAY 3/RegistrationApp.Repositories/IStudentRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using RegistrationApp.Entities;
+
+namespace RegistrationApp.Repositories
+{
+    public interface IStudentRepository
+    {
+        List<Student> GetAll();
+        void Add(Student student);
+    }
+}
---
+++ DAY 3/RegistrationApp.Repositories/StudentRepository.cs
@@ -0,0 +1,23 @@
+﻿using RegistrationApp.Entities;
+using System.Collections.Generic;
+
+namespace RegistrationApp.Repositories
+{
+    public class StudentRepository : IStudentRepository
+    {
+        private readonly List<Student> _students = new();
+
+        public List<Student> GetAll()
+        {
+            return _students;
+        }
+
+      
+        public void Add(Student student)
+        {
+            student.Id = _students.Count + 1;
+            _students.Add(student);
+        }
+
+    }
+}

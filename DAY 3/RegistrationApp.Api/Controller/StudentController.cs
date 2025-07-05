139cce3164fede20b2a457597934c21b3ad85586
Makwanajyoti <jyotimakavana221@gmail.com>
2025-07-05 16:19:19 +0530

    Add files via upload

---
+++ DAY 3/RegistrationApp.Api/Controller/StudentController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RegistrationApp.Entities;
+using RegistrationApp.Services;
+using System.Collections.Generic;
+
+namespace RegistrationApp.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class StudentController : ControllerBase
+    {
+        private readonly StudentService _studentService;
+
+        public StudentController(StudentService studentService)
+        {
+            _studentService = studentService;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Student>> Get()
+        {
+            return _studentService.GetAll();
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Student student)
+        {
+            _studentService.Add(student);
+            return Ok("Student Registered Successfully");
+        }
+    }
+}

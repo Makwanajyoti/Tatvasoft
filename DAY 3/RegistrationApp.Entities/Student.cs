ab5285cf29c6002cec3576856667662e266230cc
Makwanajyoti <jyotimakavana221@gmail.com>
2025-07-05 16:25:16 +0530

    Add files via upload

---
+++ DAY 3/RegistrationApp.Entities/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace RegistrationApp.Entities
+{
+    public class Student
+    {
+        public int Id { get; set; }  // Auto-generated
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}

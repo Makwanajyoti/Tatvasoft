1d5eeb82ce69168354a586ab645dcd55a5873d47
Makwanajyoti <jyotimakavana221@gamil.com>
2025-07-14 14:19:35 +0530

    Add Angular and ASP.NET project to DAY 8 folder

---
+++ DAY 8/backend/Mission/Mission.Entities/MissionDbContextFactory.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+
+namespace Mission.Entities
+{
+    public class MissionDbContextFactory : IDesignTimeDbContextFactory<MissionDbContext>
+    {
+        public MissionDbContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory()) // assumes appsettings.json is here
+                .AddJsonFile("appsettings.json", optional: true)
+                .Build();
+
+            var optionsBuilder = new DbContextOptionsBuilder<MissionDbContext>();
+
+            var connectionString = configuration.GetConnectionString("DefaultConnection")
+                ?? "Host=localhost;Database=mission2;Username=postgres;Password=pjyoti";
+
+            optionsBuilder.UseNpgsql(connectionString);
+
+            return new MissionDbContext(optionsBuilder.Options);
+        }
+    }
+}
